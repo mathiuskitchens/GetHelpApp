1f34719995158c2f3edbb253e8ef66fc522940cf
Matthew Kitchens <mathiuskitchens@gmail.com>
2023-09-10 17:09:40 -0400

    Initial Files

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { useState } from "react";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import MyComponent, { HomeScreen } from "./pages/HomeScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Profile from "./pages/Profile";
+import { AppRegistry } from "react-native";
+import { PaperProvider } from "react-native-paper";
+import { name as appName } from "./app.json";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={MyComponent}
+            options={{ title: "Home" }}
+          />
+          <Stack.Screen
+            name="Profile"
+            component={Profile}
+            options={{ title: "Profile" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+AppRegistry.registerComponent(appName, () => App);
---
+++ pages/Profile.jsx
@@ -0,0 +1,14 @@
+import { Text } from "react-native"
+
+
+const Profile = () => {
+  return (
+          <Text>
+          Profile Page
+          
+          </Text>
+    
+  )
+}
+
+export default Profile
